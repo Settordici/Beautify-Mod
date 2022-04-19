d0a1380bc8dee552f4237dab9452811b9970d116
Settordici <63635493+Settordici@users.noreply.github.com>
2022-04-19 19:27:48 +0200

    Revert "Added leaf_carpet (not final)"
    This reverts commit 3de9c1f0840b73069beecf38c0942f2916eb6028.

--- src/main/java/net/Settordici/beautify/world/gen/ModWorldGen.java
+++ src/main/java/net/Settordici/beautify/world/gen/ModWorldGen.java
@@ -2,8 +2,6 @@ package net.Settordici.beautify.world.gen;
 
 public class ModWorldGen {
     public static void generateModWorldGen() {
-
         ModGrassGeneration.generateGrass();
-        ModFoliageGeneration.generateFoliage();
     }
 }
